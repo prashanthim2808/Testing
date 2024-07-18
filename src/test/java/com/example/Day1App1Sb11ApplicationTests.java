ffe05d28d561f7b58eca0bdea24845b28693a56a
prashanthim2808 <prashanthim2808@gmail.com>
2024-07-18 00:13:49 -0500

    first commit

---
+++ src/test/java/com/example/Day1App1Sb11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1App1Sb11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
